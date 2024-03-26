188eda3604cb8dfb0c74e9ce15791b2e8abc088a
kimseungwoo449 <ehdanf449@naver.com>
2024-03-26 15:19:22 +0900

    add : 생성자 add : setPosition, setItems

--- zombie/src/zombie/Zombie.java
+++ zombie/src/zombie/Zombie.java
@@ -1,5 +1,26 @@
 package zombie;
 
-public class Zombie {
+public class Zombie extends Unit {
+	public Zombie() {
+		super(200, 5, 2, 10, "Zombie"); // 플레이어 초기 셋팅
+		setItems(ran.nextInt(3));
+		setPosition();
+	}
 
+	@Override
+	public void setPosition() {
+		this.position = ran.nextInt(8) + 2;
+	}
+
+	@Override
+	public void setItems(int items) {
+		this.items = items;
+	}
+//	private int setItem() {
+//		return ran.nextInt(3);
+//	}
+//	
+//	private int rPosition() {
+//		return ran.nextInt(8)+2;
+//	}
 }
