c9365780e4372cb0feeb85009a9a19210be1a0c5
kimseungwoo449 <ehdanf449@naver.com>
2024-03-26 12:48:39 +0900

    Add : Game class and setting

--- zombie/src/zombie/ZombieGame.java
+++ zombie/src/zombie/ZombieGame.java
@@ -1,5 +1,11 @@
 package zombie;
 
 public class ZombieGame {
-
+	public static void main(String[] args) {
+		ZombieGame.run();
+	}
+	
+	public static void run() {
+		Game.getInstance().run();
+	}
 }
