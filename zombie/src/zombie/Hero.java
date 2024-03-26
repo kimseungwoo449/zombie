e7a89980913bf84b6fc2a3ea094fce7fe75a6c34
kimseungwoo449 <ehdanf449@naver.com>
2024-03-26 14:31:16 +0900

    add : setLv, setMaxHp, setOffensivePower, setExp

--- zombie/src/zombie/Hero.java
+++ zombie/src/zombie/Hero.java
@@ -1,5 +1,35 @@
 package zombie;
 
-public class Hero {
-
+public class Hero extends Unit implements Playerable{
+	public Hero(int maxHp, int items, int offensivePower, int position) {
+		super(150,2,10,1,1,0,"Player");	// 플레이어 초기 셋팅
+	}
+	
+	@Override
+	public void setLv() {
+		if (super.exp >= 100) {
+			super.lv++;
+			super.exp -= 100;
+			setMaxHp();
+			setOffensivePower();
+		}
+	}
+	
+	@Override
+	public void setMaxHp() {
+		super.maxHp += 50;
+		super.currentHp = this.maxHp;
+	}
+	
+	@Override
+	public void setOffensivePower() {
+		super.offensivePower += 5;
+		super.minAttack = super.offensivePower - 10;
+		super.maxAttack = super.offensivePower + 10;
+	}
+	
+	@Override
+	public void setExp(int exp) {
+		super.exp += exp;
+	}
 }
