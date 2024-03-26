f079a50587b24f2bac9a53db30c137a3f6ea6fee
kimseungwoo449 <ehdanf449@naver.com>
2024-03-26 14:53:33 +0900

    add : interface Damageable add : damage method

--- zombie/src/zombie/Hero.java
+++ zombie/src/zombie/Hero.java
@@ -1,10 +1,10 @@
 package zombie;
 
-public class Hero extends Unit implements Playerable{
+public class Hero extends Unit implements Playerable, Damageable {
 	public Hero(int maxHp, int items, int offensivePower, int position) {
-		super(150,2,10,1,1,0,"Player");	// 플레이어 초기 셋팅
+		super(150, 2, 10, 1, 1, 0, "Player"); // 플레이어 초기 셋팅
 	}
-	
+
 	@Override
 	public void setLv() {
 		if (super.exp >= 100) {
@@ -14,22 +14,45 @@ public class Hero extends Unit implements Playerable{
 			setOffensivePower();
 		}
 	}
-	
+
 	@Override
 	public void setMaxHp() {
 		super.maxHp += 50;
 		super.currentHp = this.maxHp;
 	}
-	
+
 	@Override
 	public void setOffensivePower() {
 		super.offensivePower += 5;
 		super.minAttack = super.offensivePower - 10;
 		super.maxAttack = super.offensivePower + 10;
 	}
-	
+
 	@Override
 	public void setExp(int exp) {
 		super.exp += exp;
+	}
+
+	@Override
+	public void setItems(int items) {
+		if (super.items + items < 0) {
+			System.err.println("아이템이 모두 소진되었습니다.");
+			return;
+		}
+		super.items += items;
+	}
+
+	@Override
+	public int getItems() {
+		return super.items;
+	}
+
+	@Override
+	public int damage() {
+		int rDamage = ran.nextInt(super.minAttack) + super.maxAttack + 1;
+		int critical = ran.nextInt(4);
+		if (critical == 0)
+			rDamage *= 2;
+		return rDamage;
 	}
 }
