a2b3e121082b029d795096d79128d2ff4754584e
kimseungwoo449 <ehdanf449@naver.com>
2024-03-26 17:18:09 +0900

    add : 생성자 add : damage 메소드

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -1,5 +1,26 @@
 package zombie;
 
-public class Boss {
-
+public class Boss extends Zombie {
+	public Boss() {
+		this.lv = 5;
+		this.maxHp *= 1.5;
+		this.currentHp = this.maxHp;
+		this.exp *= 1.5;
+		this.offensivePower *= 1.5;
+		this.setAttacks();
+		this.position = 10; // 10 층보스
+		this.name = "Zombie King";
+		this.items += ran.nextInt(3);
+	}
+	
+	@Override
+	public int damage() {
+		int rDamage = ran.nextInt(super.maxAttack) + super.minAttack + 1;
+		int critical = ran.nextInt(6);	// 몹은 크리가 덜 터지게
+		if (critical == 0) {
+			System.out.printf("%s 크리티컬!!!\n",super.name);
+			rDamage *= 2;
+		}
+		return rDamage;
+	}
 }
