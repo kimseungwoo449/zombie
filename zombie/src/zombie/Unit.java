f1407a8b49fcfb287553e3b109125650463bef27
kimseungwoo449 <ehdanf449@naver.com>
2024-03-26 14:26:54 +0900

    add : interface Playerable

--- zombie/src/zombie/Unit.java
+++ zombie/src/zombie/Unit.java
@@ -2,22 +2,37 @@ package zombie;
 
 import java.util.Random;
 
-public class Unit {
+interface Playerable{
+	public void setLv();
+
+	public void setMaxHp() ;
+	
+	public void setOffensivePower() ;
+	
+	public void setExp(int exp);
+}
+
+abstract public class Unit {
 	protected Random ran = new Random();
 
-	private int currentHp; // 현재 hp
-	private int maxHp; // 최대 hp
-	private int items; // 가진 포션 수
+	protected int currentHp; // 현재 hp
+	protected int maxHp; // 최대 hp
+	protected int items; // 가진 포션 수
 
-	private int offensivePower; // 공격력
-	private int minAttack; // 최소 공격
-	private int maxAttack; // 최대 공격
+	protected int offensivePower; // 공격력
+	protected int minAttack; // 최소 공격
+	protected int maxAttack; // 최대 공격
 
-	private int position; // 위치
+	protected int position; // 위치
+
+	protected int lv; // lv
+	protected int exp; // 경험치
 
 	private String name;
 
-	protected Unit(int maxHp, int items, int offensivePower, int position, String name) {
+	private boolean isDead;
+
+	protected Unit(int maxHp, int items, int offensivePower, int position, int lv, int exp, String name) {
 		this.maxHp = maxHp;
 		this.currentHp = maxHp;
 
@@ -27,10 +42,42 @@ public class Unit {
 		this.maxAttack = this.offensivePower + 10;
 		this.position = position;
 		this.name = name;
+		this.lv = lv;
+		this.exp = exp;
+		this.isDead = false;
 	}
 
+	public int getCurrentHp() {
+		return this.currentHp;
+	}
+
+	public void setCurrentHp(int attack) {
+		this.currentHp -= attack;
+	}
+	
+	public int getPosition() {
+		return this.position;
+	}
+	
+	public void setPosition() {
+		this.position++;
+	}
+
+	public int getExp() {
+		return this.exp;
+	}
+
+	public boolean isDead() {
+		return this.isDead;
+	}
+	
+	public void dead() {
+		if(this.currentHp<=0)
+			this.isDead = true;
+	}
+	
 	@Override
 	public String toString() {
-		return String.format("%s [%d/%d]", name, currentHp, maxHp);
+		return String.format("%d. %s [%d/%d]",lv, name, currentHp, maxHp);
 	}
 }
